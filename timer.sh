5454b58907cd021607b7cfc4014e5cdb5811ff02
Matthias Dumke <matthias.dumke@gmx.net>
2020-10-30 19:01:47 +0100

    allow resolution to seconds

--- timer.sh
+++ timer.sh
@@ -2,30 +2,40 @@
 
 print_usage_info () {
   echo -e "timer - Minimalistic countdown timer\n"
-  echo -e "Usage: timer MINUTES\n"
+  echo -e "Usage: timer MINUTES SECONDS\n"
   echo -e "Available options:\n"
   echo -e "  -h,--help\tShow this help text"
   echo -e "  -t,--test\tCheck if sound works"
-  echo -e "  MINUTES\tTime to count down in minutes"
+  echo -e "  MINUTES\tNumber of minutes to count down (up to 60)"
+  echo -e "  SECONDS\tAdditional seconds to count down"
 }
 
 play_sound () {
   speaker-test -t wav -r 44100 -w $HOME/lib/timer/gong.wav -l 1 > /dev/null
 }
 
+# display usage information if needed
 if [[ $# -eq 0 ]] || [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
   print_usage_info
   exit
 fi
 
+# check for test flag
 if [[ "$1" == "--test" ]] || [[ "$1" == "-t" ]]; then
   play_sound
   exit
 fi
 
-i=$(($1 * 60))
+# ensure proper arguments
+if [[ $# -ne 2 ]]; then
+  print_usage_info
+  exit
+fi
 
-while [[ "$i" -gt 0 ]]; do
+# main countdown loop
+i=$(($1 * 60 + $2))
+
+while [[ "$i" -ge 0 ]]; do
   echo $i | awk '{printf "\rtime remaining: %s", strftime("%-M:%S", $0)}'
   i=$(( i - 1))
   sleep 1
